e5f807bc2d7e889e9487d1194edc0873970e13a9
qqlcx5 <qqlcx5@163.com>
2021-02-22 14:24:05 +0800

    目录更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,15 +4,14 @@ module.exports = {
   title: '个人博客',
   description: 'vuepress、前端、博客、个人博客',
   head: [
-    ['link', { rel: 'icon', href: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@1.1/img/1.png' }],
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@1.1/img/1.png' }],
-    ['link', { rel: 'mask-icon', href: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@1.1/img/1.png', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@1.1/img/1.png' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
-    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+    ['link', { rel: 'icon', href: 'https://cdn.jsdelivr.net/gh/qqlcx5/figure-bed@v1.0.0/image/polar_bear.jpg' }],
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
+    ['meta', {
+        name: 'keywords',
+        content: '前端博客,个人技术博客,前端,前端开发,前端框架,web前端,前端面试题,技术文档,学习,面试,JavaScript,js,ES6,TypeScript,vue,python,css3,html5,Node,git,github,markdown'},
+    ],
+    ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
+    ['script', { async: 'async', src: 'https://hm.baidu.com/hm.js?662dade0b3fe9911e80442e86935e233'}],
   ],
   themeConfig,
   plugins
